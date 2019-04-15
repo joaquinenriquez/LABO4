6f857a726ee13d3fdf02cb942f192a3f587c5c1b
Joaquin <joaquin.enriquez.tc@gmail.com>
2019-04-15 14:33:39 -0300

    nuevas cositas

---
+++ Angular/VariablesDeReferenciaDeTemplates/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  boton_click(texto: string) {
+    console.log(texto);
+  }
+
+}
+
